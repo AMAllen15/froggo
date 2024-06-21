5e7632506d27f5dd5c83a60ddb01e47bd69cb18a
AMAllen15 <allena294@gmail.com>
2024-06-20 21:25:05 -0600

    Small comment add

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -23,6 +23,7 @@ export default function Main({ navigation }: StackScreenProps<any>) {
     const [userLocation, setUserLocation] = useState<LatLng>();
     const [currentRegion, setCurrentRegion] = useState<Region>();
 
+    //Puts my location in Calgary, ab
     const user5 = {
         login: 'AMAllen15',
         avatar_url:
